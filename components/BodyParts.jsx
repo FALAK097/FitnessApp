7322410cf1ad8fa2a6b8ca2f746dfe90c2528dc4
falak <falakgala09@gmail.com>
2024-01-05 21:43:47 +0530

    production build and bodyParts

--- components/BodyParts.jsx
+++ components/BodyParts.jsx
@@ -1,10 +1,69 @@
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import { bodyParts } from '../constants';
+import { useRouter } from 'expo-router';
 
 export default function BodyParts() {
+  const router = useRouter();
   return (
-    <View>
-      <Text>BodyParts</Text>
+    <View className="mx-4">
+      <Text
+        style={{ fontSize: hp(3) }}
+        className="font-semibold text-neutral-700">
+        Exercises
+      </Text>
+
+      <FlatList
+        data={bodyParts}
+        numColumns={2}
+        keyExtractor={(item) => item.name}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50, paddingTop: 20 }}
+        columnWrapperStyle={{ justifyContent: 'space-between' }}
+        renderItem={({ item, index }) => (
+          <BodyPartCard router={router} index={index} item={item} />
+        )}
+      />
     </View>
   );
 }
+
+const BodyPartCard = ({ router, item, index }) => {
+  return (
+    <View>
+      <TouchableOpacity
+        onPress={() => router.push({ pathname: '/exercises', params: item })}
+        style={{ width: wp(44), height: wp(52) }}
+        className="flex justify-end p-4 mb-4">
+        <Image
+          source={item.image}
+          resizeMode="cover"
+          style={{ width: wp(44), height: wp(52) }}
+          className="rounded-[35px] absolute"
+        />
+        <LinearGradient
+          colors={['transparent', 'rgba(0,0,0,0.9)']}
+          style={{
+            width: wp(44),
+            height: hp(15),
+          }}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 1 }}
+          className="rounded-[35px] absolute bottom-0"
+        />
+
+        <Text
+          style={{ fontSize: hp(2.3) }}
+          className="text-white font-semibold tracking-wide text-center">
+          {item?.name}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
