02d4dbc6e5c4b401a83267c65cc2c9d1ec475b1d
falak <falakgala09@gmail.com>
2024-01-14 14:27:51 +0530

    created new navigation

---
+++ navigation/appNavigation.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignInScreen from '../auth/SignInScreen';
+import SignUpScreen from '../auth/SignUpScreen';
+import Layout from '../app/_layout';
+import Index from '../app/index';
+import Home from '../app/home';
+import Exercises from '../app/exercises';
+import ExerciseDetails from '../app/ExerciseDetails';
+
+const Stack = createStackNavigator();
+
+export const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Index"
+        screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Index" component={Index} />
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="AppLayout" component={Layout} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Exercises" component={Exercises} />
+        <Stack.Screen name="ExerciseDetails" component={ExerciseDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
