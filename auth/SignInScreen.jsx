5e35524d185dbe37fb6b3a71b635c14cce712b38
falak <falakgala09@gmail.com>
2024-01-14 15:33:49 +0530

    styling changes

--- auth/SignInScreen.jsx
+++ auth/SignInScreen.jsx
@@ -1,4 +1,11 @@
-import { View, Text, TouchableOpacity, Image, TextInput } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  KeyboardAvoidingView,
+} from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -12,8 +19,9 @@ export default function SignInScreen() {
       <SafeAreaView className="flex ">
         <View className="flex-row justify-start">
           <TouchableOpacity
-            onPress={() => navigation.goBack()}
-            className="bg-yellow-400 p-2 rounded-tr-2xl rounded-bl-2xl ml-4">
+            onPress={navigation.goBack}
+            className="bg-rose-500 mx-4 pr-1 rounded-full flex justify-center items-center absolute"
+            style={{ width: hp(5.5), height: hp(5.5), marginTop: hp(2) }}>
             <Ionicons name="caret-back-outline" size={hp(4)} color="white" />
           </TouchableOpacity>
         </View>
@@ -27,62 +35,66 @@ export default function SignInScreen() {
       <View
         style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }}
         className="flex-1 bg-white px-8 pt-8">
-        <View className="form space-y-2">
-          <Text className="text-gray-700 ml-4">Email Address</Text>
-          <TextInput
-            className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-            placeholder="email"
-            value="john@gmail.com"
-          />
-          <Text className="text-gray-700 ml-4">Password</Text>
-          <TextInput
-            className="p-4 bg-gray-100 text-gray-700 rounded-2xl"
-            secureTextEntry
-            placeholder="password"
-            value="test12345"
-          />
-          <TouchableOpacity className="flex items-end">
-            <Text className="text-gray-700 mb-5">Forgot Password?</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            className="py-3 bg-yellow-400 rounded-xl"
-            onPress={() => navigation.navigate('Home')}>
-            <Text className="text-xl font-bold text-center text-gray-700">
-              Login
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <Text className="text-xl text-gray-700 font-bold text-center py-5">
-          Or
-        </Text>
-        <View className="flex-row justify-center space-x-12">
-          <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
-            <Image
-              source={require('../assets/images/google.png')}
-              className="w-10 h-10"
+        <KeyboardAvoidingView behavior="padding">
+          <View className="form space-y-2">
+            <Text className="text-gray-700 ml-4">Email Address</Text>
+            <TextInput
+              className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
+              placeholder="email"
+              value="john@gmail.com"
+              autoCapitalize="none"
             />
-          </TouchableOpacity>
-          <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
-            <Image
-              source={require('../assets/images/apple.png')}
-              className="w-10 h-10"
+            <Text className="text-gray-700 ml-4">Password</Text>
+            <TextInput
+              className="p-4 bg-gray-100 text-gray-700 rounded-2xl"
+              secureTextEntry={true}
+              placeholder="password"
+              value="test12345"
+              autoCapitalize="none"
             />
-          </TouchableOpacity>
-          <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
-            <Image
-              source={require('../assets/images/facebook.png')}
-              className="w-10 h-10"
-            />
-          </TouchableOpacity>
-        </View>
-        <View className="flex-row justify-center mt-7">
-          <Text className="text-gray-500 font-semibold">
-            Don't have an account?
+            <TouchableOpacity className="flex items-end">
+              <Text className="text-gray-700 mb-5">Forgot Password?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="py-3 bg-yellow-400 rounded-xl"
+              onPress={() => navigation.navigate('Home')}>
+              <Text className="text-xl font-bold text-center text-gray-600">
+                Login
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <Text className="text-xl text-gray-700 font-bold text-center py-5">
+            Or
           </Text>
-          <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-            <Text className="font-semibold text-yellow-500"> Sign Up</Text>
-          </TouchableOpacity>
-        </View>
+          <View className="flex-row justify-center space-x-12">
+            <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
+              <Image
+                source={require('../assets/images/google.png')}
+                className="w-10 h-10"
+              />
+            </TouchableOpacity>
+            <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
+              <Image
+                source={require('../assets/images/apple.png')}
+                className="w-10 h-10"
+              />
+            </TouchableOpacity>
+            <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
+              <Image
+                source={require('../assets/images/facebook.png')}
+                className="w-10 h-10"
+              />
+            </TouchableOpacity>
+          </View>
+          <View className="flex-row justify-center mt-7">
+            <Text className="text-gray-500 font-semibold">
+              Don't have an account?
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+              <Text className="font-semibold text-yellow-500"> Sign Up</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
       </View>
     </View>
   );
--- auth/SignUpScreen.jsx
+++ auth/SignUpScreen.jsx
@@ -1,4 +1,11 @@
-import { View, Text, TouchableOpacity, Image, TextInput } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  KeyboardAvoidingView,
+} from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -12,80 +19,86 @@ export default function SignUpScreen() {
       <SafeAreaView className="flex ">
         <View className="flex-row justify-start">
           <TouchableOpacity
-            onPress={() => navigation.goBack()}
-            className="bg-yellow-400 p-2 rounded-tr-2xl rounded-bl-2xl ml-4">
+            onPress={navigation.goBack}
+            className="bg-rose-500 mx-4 pr-1 rounded-full flex justify-center items-center absolute"
+            style={{ width: hp(5.5), height: hp(5.5), marginTop: hp(2) }}>
             <Ionicons name="caret-back-outline" size={hp(4)} color="white" />
           </TouchableOpacity>
         </View>
         <View className="flex-row justify-center">
           <Image
             source={require('../assets/images/signup.png')}
-            style={{ width: 165, height: 110 }}
+            style={{ width: 200, height: 150 }}
           />
         </View>
       </SafeAreaView>
       <View
         style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }}
         className="flex-1 bg-white px-8 pt-8">
-        <View className="form space-y-2">
-          <Text className="text-gray-700 ml-4">Full Name</Text>
-          <TextInput
-            className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-            placeholder="text"
-            value="john doe"
-          />
-          <Text className="text-gray-700 ml-4">Email Address</Text>
-          <TextInput
-            className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-            placeholder="email"
-            value="john@gmail.com"
-          />
-          <Text className="text-gray-700 ml-4">Password</Text>
-          <TextInput
-            className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-5"
-            secureTextEntry
-            placeholder="password"
-            value="test12345"
-          />
-          <TouchableOpacity
-            className="py-3 bg-yellow-400 rounded-xl"
-            onPress={() => navigation.navigate('Home')}>
-            <Text className="text-xl font-bold text-center text-gray-700">
-              Sign Up
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <Text className="text-xl text-gray-700 font-bold text-center py-5">
-          Or
-        </Text>
-        <View className="flex-row justify-center space-x-12">
-          <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
-            <Image
-              source={require('../assets/images/google.png')}
-              className="w-10 h-10"
+        <KeyboardAvoidingView behavior="padding">
+          <View className="form space-y-2">
+            <Text className="text-gray-700 ml-4">Full Name</Text>
+            <TextInput
+              className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
+              placeholder="text"
+              value="john doe"
+              autoCapitalize="none"
             />
-          </TouchableOpacity>
-          <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
-            <Image
-              source={require('../assets/images/apple.png')}
-              className="w-10 h-10"
+            <Text className="text-gray-700 ml-4">Email Address</Text>
+            <TextInput
+              className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
+              placeholder="email"
+              value="john@gmail.com"
+              autoCapitalize="none"
             />
-          </TouchableOpacity>
-          <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
-            <Image
-              source={require('../assets/images/facebook.png')}
-              className="w-10 h-10"
+            <Text className="text-gray-700 ml-4">Password</Text>
+            <TextInput
+              className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-5"
+              secureTextEntry={true}
+              placeholder="password"
+              value="test12345"
+              autoCapitalize="none"
             />
-          </TouchableOpacity>
-        </View>
-        <View className="flex-row justify-center mt-7">
-          <Text className="text-gray-500 font-semibold">
-            Already have an account?
+            <TouchableOpacity
+              className="py-3 bg-yellow-400 rounded-xl"
+              onPress={() => navigation.navigate('Home')}>
+              <Text className="text-xl font-bold text-center text-gray-600">
+                Sign Up
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <Text className="text-xl text-gray-700 font-bold text-center py-5">
+            Or
           </Text>
-          <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
-            <Text className="font-semibold text-yellow-500"> Log In</Text>
-          </TouchableOpacity>
-        </View>
+          <View className="flex-row justify-center space-x-12">
+            <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
+              <Image
+                source={require('../assets/images/google.png')}
+                className="w-10 h-10"
+              />
+            </TouchableOpacity>
+            <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
+              <Image
+                source={require('../assets/images/apple.png')}
+                className="w-10 h-10"
+              />
+            </TouchableOpacity>
+            <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
+              <Image
+                source={require('../assets/images/facebook.png')}
+                className="w-10 h-10"
+              />
+            </TouchableOpacity>
+          </View>
+          <View className="flex-row justify-center mt-7">
+            <Text className="text-gray-500 font-semibold">
+              Already have an account?
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
+              <Text className="font-semibold text-yellow-500"> Log In</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
       </View>
     </View>
   );
