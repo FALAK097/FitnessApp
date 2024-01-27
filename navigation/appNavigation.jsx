a6cfe838469c1e5f2261e13f7deaf18412a15025
falak <falakgala09@gmail.com>
2024-01-27 16:38:31 +0530

    display loading state when user is checked for authentication to navigate to either App or Auth Stack (Added GIF)

--- navigation/appNavigation.jsx
+++ navigation/appNavigation.jsx
@@ -1,4 +1,4 @@
-import { LogBox, View, ActivityIndicator } from 'react-native';
+import { LogBox, View, ActivityIndicator, Image, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppStack, AuthStack } from './stackNavigation';
 import { useEffect, useState } from 'react';
@@ -10,36 +10,40 @@ LogBox.ignoreLogs(['Warning: Failed prop type']);
 
 const AppNavigation = () => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true); // New loading state
+  const [loading, setLoading] = useState(true);
   const auth = getAuth(FIREBASE_APP);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(
       auth,
       (user) => {
-        console.log('User:', user);
+        // console.log('User:', user);
         setUser(user);
-        setLoading(false); // Set loading to false when authentication state is determined
+        setTimeout(() => {
+          setLoading(false);
+        }, 5000);
       },
       (error) => {
         console.error('AuthStateChanged Error:', error);
-        setLoading(false); // Ensure loading state is updated even in case of error
+        setLoading(false);
       }
     );
 
     return () => unsubscribe();
   }, []);
 
-  // While loading, display an activity indicator
   if (loading) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#0000ff" />
+        <Image
+          source={require('../assets/images/loading.gif')}
+          style={{ width: '100%', height: '100%' }}
+          resizeMode="cover"
+        />
       </View>
     );
   }
 
-  // Once loading is complete, determine which stack to render based on the authentication state
   return (
     <NavigationContainer>
       {user ? <AppStack /> : <AuthStack />}
